8b52fa8fefa3b456470eb62cd12f81dba77d9ea7
Martynas <mkraujaz@gmail.com>
2025-12-17 13:15:12 +0200

    08 masyvai ir ciklai DONE update

--- PHP_MOKYMAI/08-masyvai-ir-ciklas-foreach/1-indexed-arrays/uzduotis1.php
+++ PHP_MOKYMAI/08-masyvai-ir-ciklas-foreach/1-indexed-arrays/uzduotis1.php
@@ -3,7 +3,11 @@
 // Susikurkite skaičių masyvą. Atspausdinkite pirmą, antrą ir paskutinį
 // skaičius iš šio masyvo.
 
+$numbers = [10, 20, 30, 40, 50];
 
+echo $numbers[0] . "\n";
+echo $numbers[1] . "\n";
+echo $numbers[count($numbers) - 1] . "\n";
 
 
 
--- PHP_MOKYMAI/08-masyvai-ir-ciklas-foreach/1-indexed-arrays/uzduotis2.php
+++ PHP_MOKYMAI/08-masyvai-ir-ciklas-foreach/1-indexed-arrays/uzduotis2.php
@@ -3,6 +3,15 @@
 // Susikurkite skaičių masyvą. Suskaičiuokite skaičių sumą, vidurkį.
 
 
+$skaiciai = array(5, 10, 15, 20, 25);
+$suma = 0;
+for ($i = 0; $i < count($skaiciai); $i++) {
+    $suma += $skaiciai[$i];
+}
+$vidurkis = $suma / count($skaiciai);
+echo "Suma: $suma\n";
+echo "Vidurkis: $vidurkis\n";
+
 
 
 
